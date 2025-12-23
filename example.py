944dd5e63389a0aad24159b995fd0e5b73c94cc7
Samba-T869 <Kimwagangalula45@gmail.com>
2025-12-23 15:09:20 +0300

    Add files via upload

---
+++ example.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import os
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thedaysofthejaguar'
+socketio = SocketIO(app)
+
+@app.route('/')
+def home():
+	return render_template('chat.html')
+
+@socketio.on('new_message')
+def handle_message(data):
+	sender_id = data.get('sender_id')
+	message = data.get('text')
+	
+	emit('incoming_message', {
+	'sender_id': sender_id,
+	'text': message,
+	'type': 'broadcast'
+	}, broadcast =True)
+
+if __name__ == '__main__':
+	socketio.run(app, host='0.0.0.0',port=9000, debug=True)
